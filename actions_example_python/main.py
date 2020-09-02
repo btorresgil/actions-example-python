90da89b8b943b2cce903a8e4da4cff51548beb4e
Brian Torres-Gil <brian@ixi.us>
2020-09-02 08:31:51 -0700

    Initial commit

---
+++ actions_example_python/main.py
@@ -0,0 +1,12 @@
+import xml.etree.ElementTree as ET
+from panos.firewall import Firewall
+
+
+def main() -> None:
+    fw = Firewall("10.0.1.25", "admin", "Pal0Alt0!")
+    result = fw.op("show system info")
+    print(ET.tostring(result))
+
+
+if __name__ == "__main__":
+    main()
